e4562e2ba12b99d99b82d254d49f5ac7007d1912
edwinponte <edwin.ponte@gmail.com>
2019-04-09 17:56:15 -0300

    teste commit

---
+++ soma_elementos.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 15:32:25 2017
+
+@author: Edwin
+"""
+
+def soma_elementos(lista):
+    soma = 0
+    for s in lista:
+        soma = s + soma
+    return soma
